92d28b58308744fc394aee16864ccb48decc6164
Agmar Torres <agmartorres@gmail.com>
2020-02-11 19:10:22 -0300

    add session in the application

--- app.js
+++ app.js
@@ -6,15 +6,30 @@ const handlebars = require('express-handlebars');
 const admin = require('./routes/admin.js')
 const path = require('path')
 const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('connect-flash')
+
 //Configuracoes
-const app = express()
+    const app = express()
 
     app.use(bodyParser.urlencoded({extended: false}))
     app.use(bodyParser.json())
 
     app.engine('handlebars', handlebars({defaultLayout: 'main'}))
     app.set('view engine', 'handlebars')
+//Configurações
+    //Sessão
+        app.use(session({
+            secret: 'cursodenode',
+            resave: true,
+            saveUninitialized: true
+        }))
 
+        app.use((req, res, next) =>{
+            res.locals.sucess_msg = req.flash("success_msg")
+            res.locals.error_msg = req.flash("error_msg")
+            next();
+        })
 //Banco de Dados
     //mongoose.Promise = global.Promesi;
     mongoose.connect('mongodb://localhost/blogapp',).then(
@@ -22,6 +37,11 @@ const app = express()
     ).catch(
         (err) => console.log ("Não pode ser devido a " + err)
     )
+//Middlewares
+
+    app.use((req, res, next)=>{
+        console.log("Oi eu sou um middleware")
+    })
 
 //Rotas
     app.use(express.static(path.join(__dirname, "public")))
