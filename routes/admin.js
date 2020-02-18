751b4ffbdb836fe53445638a806fd464f2652a1b
Agmar Torres <agmartorres@gmail.com>
2020-02-17 22:04:19 -0300

    adding functionality of delete

--- routes/admin.js
+++ routes/admin.js
@@ -9,11 +9,38 @@ const Categoria = mongoose.model("Categoria")
     })
     
     router.get('/', (req,res) =>{
-        res.render("admin/index")
+        Categoria.find().sort({date:'asc'}).then((categorias) =>{
+            res.render("admin/categorias", {categorias: categorias})
+        }).catch((err)=>{
+            req.flash("error_msg", "Houve um erro ao lista as categorias")
+        })    
     })
     
-    router.get('/teste', (req,res)=>{
-        res.send("Pagina")
+    router.get('/categorias/edit/:id', (req,res) => {
+            Categoria.findOne({ _id: req.params.id}).then((categoria) =>{
+            res.render("admin/editcategorias", {categoria: categoria})
+        }).catch(() =>{
+            req.flash("error_msg", "Essa categoria não existe")
+            res.redirect('/admin/categorias')
+        })
+    })
+
+    router.post("/categorias/edit", (req,res) =>{
+        console.log(teste);
+        Categoria.findOne({_id: req.body.id}).then((categoria) =>{
+            categoria.nome = req.body.nome
+            categoria.slug = req.body.slug
+            categoria.save().then(() => {
+                req.flash("success_msg", "categoria editada com sucesso")
+                res.redirect("/admin/categorias")
+            }).catch((err) => {
+                req.flash("err_msg", "Houve um erro ao tentar editar a categoria")
+                res.redirect("/admin/categorias")
+            })
+        }).catch((err)=>{
+            req.flash("error_msg", "Houve um erro ao tentar editar a categoria")
+            res.redirect("/admin/categorias")
+        })
     })
 
     router.get('/categorias', (req,res)=>{
@@ -62,5 +89,15 @@ const Categoria = mongoose.model("Categoria")
 
     })
     
+    router.post('/categorias/deletar',(req,res)=>{
+        Categoria.deleteOne({_id: req.body.id})
+        .then(()=>{
+            req.flash("success_msg", "Mensagem excluída com sucesso")
+            res.redirect("/admin/categorias")
+        }).catch((err)=>{
+            req.flash("error_msg", "Houve um erro ao tentar a categoria")
+            res.redirect("/admin/categorias")
+        })
+    })
     
 module.exports = router
