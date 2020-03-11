ed08b5bf10339551f38c714b2fb8a370c32e7827
Agmar Torres <agmartorres@gmail.com>
2020-03-11 10:28:52 -0300

    adding model Usuario

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const path = require("path");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const flash = require("connect-flash");
-require("./models/Postagem");
 
+require("./models/Postagem");
 const Postagem = mongoose.model("Postagem");
+
+require("./models/Categoria")
+const Categoria = mongoose.model("Categoria")
+
 
 //Configuracoes
 const app = express();
@@ -69,21 +73,50 @@ app.get("/404", (req, res) => {
 app.get("/postagem/:slug", (req, res) => {
   Postagem.findOne({ slug: req.params.slug })
     .then(postagem => {
-      if (postagem) {  
+      if (postagem) {
         res.render("postagem/index", { postagem: postagem });
       } else {
         req.flash("msg_error", "Houve um erro para carregar essa postagem");
-        //res.redirect("/");
+        res.redirect("/");
       }
     })
     .catch(error => {
-      req.flash(
-        "msg_error",
-        "Houve um erro para carregar essa postagem"
-      );
+      req.flash("msg_error", "Houve um erro para carregar essa postagem");
       res.redirect("/");
     });
 });
+
+app.get("/categorias", (req, res) => {
+  Categoria.find().then(categorias =>{
+    res.render("categorias/index", { categorias: categorias})
+    
+  }).catch(error =>{
+    req.flash("error_msg", " Houve um erro interno ao lsitas as categorias")
+    res.redirect("/")
+  })
+})
+
+app.get("/categorias/:slug", (req, res) =>{
+  Categoria.findOne({ slug: req.params.slug }).then(categoria =>{
+    if(categoria){
+      Postagem.find({ categoria: categoria._id}).then( postagem =>{
+        res.render("categorias/postagens", { postagens: postagem, categoria: categoria })
+        
+      }).catch(error => {
+        req.flash("error_msg", "Houve um erro ao encontrar as postagens")
+        res.redirect("/")
+      }) // pesquisar os posts quem tem essa categoria
+      
+    }else{
+      req.flash("error_msg", "Essa categoria nao existe")
+      res.redirect("/")
+    }
+  }).catch( (error) => {
+    req.flash("error_msg", "Houve um erro interno ao recarregar a pagina dessa categoria")
+    res.redirect("/")
+  })
+})
+
 //Servidor
 
 const porta = 8081;
